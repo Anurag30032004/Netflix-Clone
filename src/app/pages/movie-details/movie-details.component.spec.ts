90ca6b252f58edcb29a1fc471827ee78ec096247
mounishvatti <mounishvatti2002@gmail.com>
2023-10-29 23:02:31 +0530

    updated testcase

--- src/app/pages/movie-details/movie-details.component.spec.ts
+++ src/app/pages/movie-details/movie-details.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieDetailsComponent } from './movie-details.component';
+import { By } from '@angular/platform-browser';
 
 describe('MovieDetailsComponent', () => {
   let component: MovieDetailsComponent;
@@ -19,5 +20,19 @@ describe('MovieDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display movie title', () => {
+    component.getMovieDetailResult = { original_title: 'Test Movie' };
+    fixture.detectChanges();
+    const titleElement = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(titleElement.textContent).toContain('Test Movie');
+  });
+
+  it('should display movie overview', () => {
+    component.getMovieDetailResult = { overview: 'Test Overview' };
+    fixture.detectChanges();
+    const overviewElement = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(overviewElement.textContent).toContain('Test Overview');
   });
 });
